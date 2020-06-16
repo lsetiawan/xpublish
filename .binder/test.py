1db3cb4487b5ef53687e6d6e7b0adc0a26911f10
Landung Setiawan <landungs@uw.edu>
2020-06-16 16:46:03 -0700

    Add example of optional route use

--- .binder/test.py
+++ .binder/test.py
@@ -2,6 +2,8 @@ import xarray as xr
 from dask.distributed import Client
 
 import xpublish  # noqa: F401
+from xpublish.routers import APIRouterWrapper
+from fastapi import Depends
 
 if __name__ == "__main__":
 
@@ -13,3 +15,26 @@ if __name__ == "__main__":
     print(ds)
 
     ds.rest.serve()
+
+    # === Multiple datasets =================
+    # class DatasetRouters(APIRouterWrapper):
+    #     """A simple example."""
+
+    #     def init_router(self):
+    #         super().init_router()
+
+    #         @self._router.get("/dims")
+    #         def get_dims(dataset: xr.Dataset = Depends(self.dataset)):
+    #             return dataset.dims
+
+    # routers = DatasetRouters(
+    #     {
+    #         'air_temp': xr.tutorial.open_dataset(
+    #             "air_temperature", chunks=dict(lat=5, lon=5), decode_cf=False
+    #         ),
+    #         'rasm': xr.tutorial.open_dataset("rasm", decode_cf=False),
+    #     }
+    # )
+
+    # xr.Dataset.rest(routers=routers).serve()
+    # ========================================
